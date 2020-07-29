51f252ff544e34978cd9fd95c3bc80653fb099ae
tomas <38633386+Tomasroma64@users.noreply.github.com>
2020-07-29 14:09:24 +0200

    Procfile and port

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
     console.log('http://localhost:' + port);
 });
